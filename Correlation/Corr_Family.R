3e3eb65b99c1c79e75abe3f0a4df232db3668085
kunlin0814 <abc730814@gmail.com>
2019-08-31 21:07:59 -0400

    Create Corr_Family.R
    Calculate diversity or correlation

---
+++ Correlation/Corr_Family.R
@@ -0,0 +1,45 @@
+library(minerva)
+
+conn <- file("/scratch/kh31516/TCGA/Stomach_original/Stomach/CORR/Family/blood",open="r")
+linn <-readLines(conn)
+result<-cbind("Family", "MIC", "spearman","pVspearman","pearson","pVpearson","Case")
+write.table(result,append = TRUE,col.names=F,row.names=F,sep="\t",quote=F,file="/scratch/kh31516/TCGA/Stomach_original/Stomach/CORR/Family/blood.result")
+for (i in 1:length(linn)){
+	Family=unlist(strsplit(linn[i], "[.]"))[1]
+	dat <- read.table(paste("/scratch/kh31516/TCGA/Stomach_original/Stomach/CORR/",linn[i],sep=""), header=T)
+	x <- dat[,2]
+	y <- dat[,3]
+	M<-mine(x, y)$MIC
+	S<-cor(x,y, method="spearman")
+	Sp<-cor.test(x,y,method="spearman")$p.value
+	P<-cor(x,y, method="pearson")
+	Pp<-cor.test(x,y,method="pearson")$p.value
+	count<-max(colSums( dat[,c(2,3)]!= 0))
+	result<-cbind(Family,M,S,Sp,P,Pp,count)
+	write.table(result,append = TRUE,col.names=F,row.names=F,sep="\t",quote=F,file="/scratch/kh31516/TCGA/Stomach_original/Stomach/CORR/Family/blood.result")
+}
+
+close(conn)
+
+
+conn <- file("/scratch/kh31516/TCGA/Stomach_original/Stomach/CORR/Family/adjacent",open="r")
+linn <-readLines(conn)
+result<-cbind("Family", "MIC", "spearman","pVspearman","pearson","pVpearson","Case")
+write.table(result,append = TRUE,col.names=F,row.names=F,sep="\t",quote=F,file="/scratch/kh31516/TCGA/Stomach_original/Stomach/CORR/Family/adjacent.result")
+for (i in 1:length(linn)){
+	Family=unlist(strsplit(linn[i], "[.]"))[1]
+	dat <- read.table(paste("/scratch/kh31516/TCGA/Stomach_original/Stomach/CORR/",linn[i],sep=""), header=T)
+	x <- dat[,2]
+	y <- dat[,3]
+	M<-mine(x, y)$MIC
+	S<-cor(x,y, method="spearman")
+	Sp<-cor.test(x,y,method="spearman")$p.value
+	P<-cor(x,y, method="pearson")
+	Pp<-cor.test(x,y,method="pearson")$p.value
+	count<-max(colSums( dat[,c(2,3)]!= 0))
+	result<-cbind(Family,M,S,Sp,P,Pp,count)
+	write.table(result,append = TRUE,col.names=F,row.names=F,sep="\t",quote=F,file="/scratch/kh31516/TCGA/Stomach_original/Stomach/CORR/Family/adjacent.result")
+}
+
+close(conn)
+
