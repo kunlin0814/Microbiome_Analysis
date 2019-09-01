068774b57d8c26b225064f02e1c5457c79fbbab6
kunlin0814 <abc730814@gmail.com>
2019-08-31 21:08:23 -0400

    Create diversity_family.py

---
+++ diversity_family.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 13 14:05:02 2019
+
+@author: kun-linho
+"""
+
+import sys
+import math
+file_name=sys.argv[1]
+total_reads_file=sys.argv[2]
+
+with open (total_reads_file, 'r')as f1:
+    file1=f1.read()
+    
+total_read=int(file1)   
+
+with open (file_name, 'r')as f:
+    file=f.read()
+
+total=file.split('\n')[:-1]
+score={}
+for i in range(len(total)):
+    name=total[i].split()[0]
+    value=total[i].split()[1]
+    score[name]=value
+sum=0
+
+for i in score.values():
+    sum+=int(i)
+
+family_enrichment=(sum/total_read)*1000000
+ni=0
+Shannon=0
+denominator=sum*(sum-1)
+for i in score.values():
+    ni+=int(i)*(int(i)-1)
+    Shannon+=(int(i)/sum)*math.log2(int(i)/sum)
+Shannon = -1 * Shannon
+#Simpson=1-(ni/denominator)
+
+
+
+output=open('diversity_calculation_family' + '.txt' ,'w')
+output.write(total_reads_file+'\t'+'total_read'+'\t'+'total_family_count'+'\t'+'family_enrichment'+'\t'+'Shannon'+'\n')
+output.write(total_reads_file+'\t'+str(total_read)+'\t'+str(sum)+'\t'+str(family_enrichment)+'\t'+str(Shannon)+'\n')
+output.close()
