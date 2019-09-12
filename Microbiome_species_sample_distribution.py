92cef154a98ef658141ec336dae86ad22ac6821d
kunlin0814 <abc730814@gmail.com>
2019-09-12 11:31:57 -0400

    Update Microbiome_species_sample_distribution.py
    Modify the format and comment

--- Microbiome_species_sample_distribution.py
+++ Microbiome_species_sample_distribution.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# The file we need
+# 1. we need to find total sort-fill0 file as our first input( to get the file name for each fill0)
+# 2. we need to find the overlap species between TCGA and Gtex derived from Gtex_TCGA.overlap script
+
+# This script will produce three files. 
+# 1. Every species enrichmnet for all samples. 2. Samples didn't pass the total species enrichment. 3 Samples that pass each species enrichemnt    
+
+# We can specify the thershold count for total species enrichment, 
+# if we only need to find the distribution, we can put 0. Once, we decide the threshold (FPM), we can replace it
+# we can also decide the cutoff for each species 
+# 
+
 import sys
 
 with open ('/scratch/kh31516/TCGA/Stomach_original/Stomach/source/total_blood_TCGA_species_fill0.txt' ,'r') as f:
     total_file = f.read()
     total_name = total_file.split('\n')[:-1]
 
-## here we need to find out what are the species that share for both TCGA and Gtex
-with open ('/scratch/kh31516/TCGA/Stomach_original/Stomach/source/Species_shared_TCGA_Gtex.txt','r') as f:
-    share_species=f.read()
-    total_share_species = share_species.split('\n')[:-1]
 
 each_file={} # here we want to see the total species enrichment in each file (FPM)
 for file_name in total_name:
@@ -25,10 +33,15 @@ for file_name in total_name:
                 species_count += int(TCGA_species_file[i].split()[1])
                 each_file[each_file_name]=float(species_count*1000000/total_reads)
 
-threshold_count = [0]
+## here we need to find out what are the species that share for both TCGA and Gtex
+with open ('/scratch/kh31516/TCGA/Stomach_original/Stomach/source/Species_shared_TCGA_Gtex.txt','r') as f:
+    share_species=f.read()
+    total_share_species = share_species.split('\n')[:-1]
+
+threshold_count = [0] # the threshold_count here is the total species enrichment
 for j in threshold_count :
     pass_files = []
-    file_output = open('/scratch/kh31516/TCGA/Stomach_original/Stomach/threshold_count_'+str(j)+'file_no_pass_summary.txt','w')
+    file_output = open('/scratch/kh31516/TCGA/Stomach_original/Stomach/files_didt_pass_threshold_count_'+str(j)+'_summary.txt','w')
     for i in each_file.keys():
         if each_file[i] > int(j):
             pass_files.append(i)
@@ -37,11 +50,9 @@ for j in threshold_count :
             threshold = str(didt_pass)+'\t'+"don't pass the threshold:\t" + str(j)
             file_output.write(str(threshold)+'\t'+ 'the_species_enrich is:\t' + str(each_file[i]) +'\n')
     summary={}
-    file_species_summary = open('/scratch/kh31516/TCGA/Stomach_original/Stomach/TCGA_blood_Species_summary.txt','w')
-    Species_With_cutoff =  open ('/scratch/kh31516/TCGA/Stomach_original/Stomach/TCGA_blood_Species_with_cutoff.txt','w')            
+    file_species_summary = open('/scratch/kh31516/TCGA/Stomach_original/Stomach/TCGA_blood_allSpecies_summary.txt','w')
+    Species_With_cutoff =  open('/scratch/kh31516/TCGA/Stomach_original/Stomach/TCGA_blood_Species_with_cutoff.txt','w')            
     for pass_file in pass_files:
-        #output=open('Blood_TCGA_threshold_count_'+str(j)+'species_ratio_summary' + '.txt' ,'w')
-        #file_species_summary = open('total_file_Pseuo_flu_summary.txt','w')
         pass_file_name = pass_file.split("HumanMicroBiome")[0].split('/')[-2]
         with open ('/scratch/kh31516/TCGA/Stomach_original/Stomach/results/'+pass_file_name+'/'+pass_file_name+'-TotalReads','r') as f1:
             new_total_reads=int(f1.read())
@@ -49,7 +60,7 @@ for j in threshold_count :
             score ={}
             overlap={}
             cut_off_species={}
-            species_cut_off = float(3.36/112)
+            species_cut_off = float(0) # Here we can specify the cutoff for each species, if we don't have, we can use 0
             TCGA_species_file= f.read().split('\n')[:-1]
             for i in range(len(TCGA_species_file)):
                 name = TCGA_species_file[i].split()[0]
@@ -59,33 +70,36 @@ for j in threshold_count :
                 overlap[i] = float(int(score[i])* 1000000/ new_total_reads)
            
             for i in total_share_species : 
-                if int(overlap[i]) > species_cut_off:
+                if int(overlap[i]) >= species_cut_off:
                     cut_off_species[i] =  float(overlap[i])
-                else :
-                    print(str(i)+" didn't pass cutoff")
+                #else :
+                    #print(str(i)+" didn't pass cutoff")
             
-            Species_With_cutoff.write('\t')
-            file_species_summary.write('\t')
+        Species_With_cutoff.write('\t')
+        file_species_summary.write('\t')
 
-            for key in overlap.keys():
-                #print(key)
-                file_species_summary.write(str(key)+'\t')
-            file_species_summary.write('\n') 
-            file_species_summary.write(str(pass_file_name)+'\t')
-            for key in overlap.keys():
-                file_species_summary.write(str(overlap[key])+'\t')
+        for key in overlap.keys():
+            #print(key)
+            file_species_summary.write(str(key)+'\t')
+        
+        file_species_summary.write('\n') 
+        file_species_summary.write(str(pass_file_name)+'\t')
+        for key in overlap.keys():
+            file_species_summary.write(str(overlap[key])+'\t')
+        
+        file_species_summary.write('\n')
 
-            for key in cut_off_species.keys():
-                #print(key)
-                Species_With_cutoff.write(str(key)+'\t')
-            Species_With_cutoff.write('\n') 
-            Species_With_cutoff.write(str(pass_file_name)+'\t')
-            for key in cut_off_species.keys():
-                Species_With_cutoff.write(str(cut_off_species[key])+'\t')
+        for key in cut_off_species.keys():
+            #print(key)
+            Species_With_cutoff.write(str(key)+'\t')
+        Species_With_cutoff.write('\n') 
+        Species_With_cutoff.write(str(pass_file_name)+'\t')
+        for key in cut_off_species.keys():
+            Species_With_cutoff.write(str(cut_off_species[key])+'\t')
 
-            Species_With_cutoff.write('\n')
-            #for key in overlap.keys():
-            #file_species_summary.write(str(key)+'\t')  
+        Species_With_cutoff.write('\n')
+        #for key in overlap.keys():
+        #file_species_summary.write(str(key)+'\t')  
 file_output.close()
 file_species_summary.close()
 Species_With_cutoff.close()    
