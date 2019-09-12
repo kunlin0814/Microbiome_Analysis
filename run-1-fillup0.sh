f55d4b41a554255486c7648e0dd7c8937c84fcc4
kunlin0814 <abc730814@gmail.com>
2019-09-12 16:18:13 -0400

    change

--- run-1-fillup0.sh
+++ run-1-fillup0.sh
@@ -13,12 +13,12 @@ while read line
 		cat $line.sam-readsID-PhylumFamilySpecies-SpeciesSum|sort > $line.sam-readsID-PhylumFamilySpecies-SpeciesSum-sort
 		cat $line.sam-readsID-PhylumFamilySpecies-PhylumSum|sort > $line.sam-readsID-PhylumFamilySpecies-PhylumSum-sort
 		cat $line.sam-readsID-PhylumFamilySpecies-FamilySum|sort > $line.sam-readsID-PhylumFamilySpecies-FamilySum-sort
-		python /scratch/kh31516/TCGA/colon/Scripts_for_data_anlaysis/prepare-matrix.py /scratch/kh31516/TCGA/colon/source/Total_Uniq_Species.txt $line.sam-readsID-PhylumFamilySpecies-SpeciesSum-sort $line.sam-readsID-PhylumFamilySpecies-SpeciesSum-sort-fill0
-		python /scratch/kh31516/TCGA/colon/Scripts_for_data_anlaysis/prepare-matrix.py /scratch/kh31516/TCGA/colon/source/Total_Uniq_Phylum.txt  $line.sam-readsID-PhylumFamilySpecies-PhylumSum-sort $line.sam-readsID-PhylumFamilySpecies-PhylumSum-sort-fill0
-		python /scratch/kh31516/TCGA/colon/Scripts_for_data_anlaysis/prepare-matrix.py /scratch/kh31516/TCGA/colon/source/Total_Uniq_Family.txt $line.sam-readsID-PhylumFamilySpecies-FamilySum-sort $line.sam-readsID-PhylumFamilySpecies-FamilySum-sort-fill0
+		python /scratch/kh31516/TCGA/colon/Scripts_for_data_anlaysis/prepare-matrix.py /scratch/kh31516/TCGA/colon/source/Total_colon_Uniq_Species.txt $line.sam-readsID-PhylumFamilySpecies-SpeciesSum-sort $line.sam-readsID-PhylumFamilySpecies-SpeciesSum-sort-fill0
+		python /scratch/kh31516/TCGA/colon/Scripts_for_data_anlaysis/prepare-matrix.py /scratch/kh31516/TCGA/colon/source/Total_colon_Uniq_Phylum.txt  $line.sam-readsID-PhylumFamilySpecies-PhylumSum-sort $line.sam-readsID-PhylumFamilySpecies-PhylumSum-sort-fill0
+		python /scratch/kh31516/TCGA/colon/Scripts_for_data_anlaysis/prepare-matrix.py /scratch/kh31516/TCGA/colon/source/Total_colon_Uniq_Family.txt $line.sam-readsID-PhylumFamilySpecies-FamilySum-sort $line.sam-readsID-PhylumFamilySpecies-FamilySum-sort-fill0
 		#type=$(echo $line |cut -d'-' -f1-3)
 		#cp $line.sam-readsID-PhylumFamilySpecies-SpeciesSum-sort-fill0 /scratch/kh31516/TCGA/colon/source/Corr/$type/$line.sam-readsID-PhylumFamilySpecies-SpeciesSum-sort-fill0
 		#cp $line.sam-readsID-PhylumFamilySpecies-PhylumSum-sort-fill0 /scratch/kh31516/TCGA/colon/source/Corr/$type/$line.sam-readsID-PhylumFamilySpecies-PhylumSum-sort-fill0
 		#cp $line.sam-readsID-PhylumFamilySpecies-FamilySum-sort-fill0 /scratch/kh31516/TCGA/colon/source/Corr/$type/$line.sam-readsID-PhylumFamilySpecies-FamilySum-sort-fill0
-	done < /scratch/kh31516/TCGA/colon/source/total_colon_blood_folders.txt
+	done < /scratch/kh31516/TCGA/colon/results/Blood/total_colon_blood_cases.txt
 
