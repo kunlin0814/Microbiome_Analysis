1669cf9545a7b48c5ae1f9d84964caea2497b6f3
kunlin0814 <abc730814@gmail.com>
2019-10-10 17:56:07 -0400

    Gtex

--- Find_coverage/find_coverage_colon.sh
+++ Find_coverage/find_coverage_colon.sh
@@ -7,17 +7,20 @@
 #PBS -M kh31516@uga.edu
 #PBS -m ae
 
-cd /scratch/kh31516/TCGA/colon/results/Blood/
+cd /scratch/kh31516/Gtex/Blood/WGS_normal_blood_result/results/
 module load gdc-client/1.3.0
 module load SAMtools/1.6-foss-2016b
 
-printf "%s\t%s\t%s\t%sf\n" "fileName" "seq_Len" "totalReads" "seqDepth" >> /scratch/kh31516/TCGA/colon/colonBloodSeqCoverageSummary.txt
+printf "%s\t%s\t%s\t%sf\n" "fileName" "seq_Len" "totalReads" "seqDepth" >> /scratch/kh31516/Gtex/Blood/WGS_normal_blood_result/Gtex_blood_coverage_summary.txt
 while read line;
 do 
-    cd /scratch/kh31516/TCGA/colon/results/Blood/$line
-    samtools view ${line}.unmapped_sorted.bam | head -10 | cut -f10  > /scratch/kh31516/TCGA/colon/results/Blood/$line/length.txt
-    length=$(python3 /scratch/kh31516/TCGA/Stomach_original/Stomach/find_bam_seq_len.py /scratch/kh31516/TCGA/colon/results/Blood/$line/length.txt | bc)
+    cd /scratch/kh31516/Gtex/Blood/WGS_normal_blood_result/results/$line
+    #rm /scratch/kh31516/Gtex/Blood/WGS_normal_blood_result/results/$line/length.txt
+    Name=$(echo /scratch/kh31516/Gtex/Blood/WGS_normal_blood_result/results/$line/*[0-9].bam)
+    #echo "$Name"
+    samtools view $Name | head -10 | cut -f10  > /scratch/kh31516/Gtex/Blood/WGS_normal_blood_result/results/$line/length.txt
+    length=$(python3 /scratch/kh31516/TCGA/Stomach_original/Stomach/find_bam_seq_len.py /scratch/kh31516/Gtex/Blood/WGS_normal_blood_result/results/$line/length.txt | bc)
     total=$(cat ${line}-TotalReads | bc)
     coverage=$((length*total/3000000000))
-    printf  "%s\t%d\t%d\t%4f\n" "${line}" "$length" "$total" "$coverage" >> /scratch/kh31516/TCGA/colon/colonBloodSeqCoverageSummary.txt
-done < /scratch/kh31516/TCGA/colon/source/total_colon_bloodCases.txt
+    printf  "%s\t%d\t%d\t%4f\n" "${line}" "$length" "$total" "$coverage" >> /scratch/kh31516/Gtex/Blood/WGS_normal_blood_result/Gtex_blood_coverage_summary.txt
+done < /scratch/kh31516/Gtex/Blood/WGS_normal_blood_result/source/Total_gtex_blood_cases.txt
