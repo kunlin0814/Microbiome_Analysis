17f56c3bdea30d86d77905ceb7bc021750f936fa
kunlin0814 <abc730814@gmail.com>
2019-10-24 21:05:42 -0400

    Update find_coverage_STC.sh
    add cut off and moving the file to different location

--- Find_coverage/find_coverage_STC.sh
+++ Find_coverage/find_coverage_STC.sh
@@ -11,7 +11,8 @@ cd /scratch/kh31516/TCGA/Stomach_original/Stomach/results
 module load gdc-client/1.3.0
 module load SAMtools/1.6-foss-2016b
 
-printf "%s\t%s\t%s\t%sf\n" "fileName" "seq_Len" "totalReads" "seqDepth" >> /scratch/kh31516/TCGA/Stomach_original/Stomach/seqDepthSummary.txt
+cut_off=$((10))
+printf "%s\t%s\t%s\t%sf\n" "fileName" "seq_Len" "totalReads" "seqDepth" >> /scratch/kh31516/TCGA/Stomach_original/Stomach/gt_cutoffseqDepthSummary.txt
 while read line;
 do 
     cd /scratch/kh31516/TCGA/Stomach_original/Stomach/results/$line
@@ -19,5 +20,8 @@ do
     length=$(python3 /scratch/kh31516/TCGA/Stomach_original/Stomach/find_bam_seq_len.py /scratch/kh31516/TCGA/Stomach_original/Stomach/results/$line/length.txt | bc)
     total=$(cat ${line}-TotalReads | bc)
     coverage=$((length*total/3000000000))
-    printf  "%s\t%d\t%d\t%2f\n" "${line}" "$length" "$total" "$coverage" >> /scratch/kh31516/TCGA/Stomach_original/Stomach/seqDepthSummary.txt
+    if [ $coverage -ge $cut_off ]; then
+        printf  "%s\t%d\t%d\t%.4f\n" "${line}" "$length" "$total" "$coverage" >> /scratch/kh31516/TCGA/Stomach_original/Stomach/gt_cutoffseqDepthSummary.txt
+        cp -r /scratch/kh31516/TCGA/Stomach_original/Stomach/results/$line /scratch/kh31516/TCGA/Stomach_original/Stomach/gt_cutoff
+    fi
 done < /scratch/kh31516/TCGA/Stomach_original/Stomach/source/TCGA_STC_cases_inclus.txt
