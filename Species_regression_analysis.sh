2eb78d236e3c94a4be74d16162b3e4682890bfcc
kunlin0814 <abc730814@gmail.com>
2019-10-31 19:44:37 -0400

    Update Species_regression_analysis.sh
    put common line argumnet

--- Species_regression_analysis.sh
+++ Species_regression_analysis.sh
@@ -13,7 +13,8 @@ module load R/3.4.4-foss-2016b-X11-20160819-GACRC
 while read line 
 do
     check=$(ls /scratch/kh31516/TCGA/CRC/gt_cutoff/CORR/Species | grep -i "${line}.blood")
-    R CMD BATCH reg_analysis.R $check
+    #R CMD BATCH 
+    Rscript --vanilla /scratch/kh31516/TCGA/CRC/gt_cutoff/CORR/reg_analysis.R /scratch/kh31516/TCGA/CRC/gt_cutoff/CORR/Species/$check
 done < /scratch/kh31516/TCGA/CRC/source/CRC_candidate_speceis.txt
 #R CMD BATCH Corr_Species.R
 #R CMD BATCH Corr_Family.R
