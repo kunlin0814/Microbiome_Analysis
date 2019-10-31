0e1d4872aed477a4d7a7e81d3fb214111b434a7c
kunlin0814 <abc730814@gmail.com>
2019-10-31 17:53:12 -0400

    add plot

---
+++ Species_regression_analysis.sh
@@ -0,0 +1,19 @@
+#PBS -S /bin/bash
+#PBS -q batch
+#PBS -l nodes=1:ppn=1:AMD
+#PBS -N corr_microbiome
+#PBS -l walltime=48:00:00
+#PBS -l mem=20gb
+#PBS -M kh31516@uga.edu 
+#PBS -m ae
+
+
+cd /scratch/kh31516/TCGA/CRC/gt_cutoff/CORR/Species
+module load R/3.4.4-foss-2016b-X11-20160819-GACRC
+while read line 
+do
+    check=$(ls /scratch/kh31516/TCGA/CRC/gt_cutoff/CORR/Species | grep -i "${line}.blood")
+    R CMD BATCH reg_analysis.R $check
+done < /scratch/kh31516/TCGA/CRC/source/CRC_candidate_speceis.txt
+#R CMD BATCH Corr_Species.R
+#R CMD BATCH Corr_Family.R
---
+++ reg_analysis.R
@@ -0,0 +1,39 @@
+library(minerva)
+library(ggplot2)
+args    <- commandArgs(trailingOnly=TRUE)
+# if (length(args)==0) {
+#   stop("At least one argument must be supplied (input file).n", call.=FALSE)
+input_file <- args[1]
+  #'Aspergillus_fumigatus.blood'
+  #args[1]
+#output_file <- args[2]
+data <- read.table(input_file,
+                   sep ='\t',
+                   header = T,
+                   stringsAsFactors = F)
+
+file_name <- input_file
+Species <- unlist(strsplit(file_name,split='/'))[9]
+x <-  data$X01
+y <-  data$X10
+S<-cor(x,y, method="spearman")
+Sp<-cor.test(x,y,method="spearman")$p.value
+P<-cor(x,y, method="pearson")
+Pp<-cor.test(x,y,method="pearson")$p.value
+          
+#data$CaseID <- as.factor(data$CaseID)
+#data$X10 <- as.factor(data$X10)
+png(paste("/scratch/kh31516/TCGA/CRC/gt_cutoff/CORR/",Species,".png",sep = ""),width=3000,height=2400,res=300)
+ggplot(data, aes(x=X01, y=X10)) + 
+  ggtitle(Species)+
+  geom_point(size=2)+
+  xlab("Tumor samples") +
+  ylab("Blood_Samples") +
+  annotate(geom="text", x=5, y=max(y), label=paste("Spearman",S,sep = " = "), color="blue")+
+  annotate(geom="text", x=5, y=max(y)-1, label=paste("S-pvalue",Sp,sep = " = "), color="red")+
+  annotate(geom="text", x=5, y=max(y)-2, label=paste("Pearson",P,sep = " = "), color="blue")+
+annotate(geom="text", x=5, y=max(y)-3, label=paste("P-pvalue",Pp,sep = " = "), color="red")+
+  theme(axis.text=element_text(size=14),
+    axis.title=element_text(size=14,face="bold"),
+    plot.title = element_text(color = "black", size = 20, face = "bold"))
+dev.off()
