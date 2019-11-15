2b4c41500bcbf7d733accc200b40d653003b4af4
kunlin0814 <abc730814@gmail.com>
2019-11-15 15:47:53 -0500

    auto plot all

--- auto_all_candidate_reg_analysis.R
+++ auto_all_candidate_reg_analysis.R
@@ -1,19 +1,27 @@
 library(minerva)
 library(ggplot2)
-args    <- commandArgs(trailingOnly=TRUE)
+#args    <- commandArgs(trailingOnly=TRUE)
 # if (length(args)==0) {
 #   stop("At least one argument must be supplied (input file).n", call.=FALSE)
-input_file <- args[1]
+#input_file <- args[1]
   #'Aspergillus_fumigatus.blood'
   #args[1]
 #output_file <- args[2]
-data <- read.table(input_file,
-                   sep ='\t',
-                   header = T,
-                   stringsAsFactors = F)
 
-file_name <- input_file
-Species <- unlist(strsplit(file_name,split='/'))[8]
+for (category in categories) {
+Df  <- paste("CRC", ".", "Candidate",category,".pdf", sep="", collapse="");
+pdf(file=Df, w=16, h=9)
+categories <- c("Species","Family","Phylum")
+candidate_microbes <- read.table(paste("/Volumes/Research_Data/Microbiome_analysis/CRC_combine_with_cufOff/Candidate_species_family_phylum_data/CRC_candidate_",category,".txt",sep=""))
+#file_name <- input_file
+candidates <- candidate_microbes$V1
+
+for (candidate in candidates){
+data <- read.table(paste("/Volumes/Research_Data/Microbiome_analysis/CRC_combine_with_cufOff/Candidate_microbe/",category,"/","CORR","/",candidate,".blood",sep=""),
+                     sep ='\t',
+                     header = T,
+                     stringsAsFactors = F)
+
 x <-  data$X01
 y <-  data$X10
 S<-cor(x,y, method="spearman")
@@ -23,12 +31,12 @@ Pp<-cor.test(x,y,method="pearson")$p.value
           
 #data$CaseID <- as.factor(data$CaseID)
 #data$X10 <- as.factor(data$X10)
-png(paste("/scratch/kh31516/TCGA/CRC/gt_cutoff/CORR/",Species,".png",sep = ""),width=3000,height=2400,res=300)
-ggplot(data, aes(x=X01, y=X10)) + 
-  ggtitle(Species)+
+#png(paste(category,"_",candidate,".png",sep = ""),width=3000,height=2400,res=300)
+plot_figure <- ggplot(data, aes(x=X01, y=X10)) + 
   geom_point(size=2)+
   xlab("Tumor samples") +
   ylab("Blood_Samples") +
+  ggtitle(paste("Correlation_of",category,candidate,sep="_"))+
   annotate(geom="text", x=0.25*max(x), y=max(y), label=paste("Spearman",S,sep = " = "), color="blue")+
   annotate(geom="text", x=0.25*max(x), y=max(y)-(0.3*max(y)*0.3), label=paste("S-pvalue",Sp,sep = " = "), color="red")+
   annotate(geom="text", x=0.25*max(x), y=max(y)-(0.3*max(y)*0.6), label=paste("Pearson",P,sep = " = "), color="blue")+
@@ -36,6 +44,10 @@ ggplot(data, aes(x=X01, y=X10)) +
   theme(axis.text=element_text(size=14),
     axis.title=element_text(size=14,face="bold"),
     plot.title = element_text(color = "black", size = 20, face = "bold"))
-dev.off()
+plot(plot_figure)
 
+}
+dev.off()
+}
 #-(0.3*max(y)0.6)
+
---
+++ single_regression_analysis.R
@@ -0,0 +1,42 @@
+library(minerva)
+library(ggplot2)
+
+draw_correlation_plot <- function(category,candidate, x_range,y_range){
+data <- read.table(paste("/Volumes/Research_Data/Microbiome_analysis/CRC_combine_with_cufOff/Candidate_microbe/",category,"/CORR/",candidate,".blood",sep=""),
+                   sep ='\t',
+                   header = T,
+                   stringsAsFactors = F)
+
+x <-  data$X01
+y <-  data$X10
+S<-cor(x,y, method="spearman")
+Sp<-cor.test(x,y,method="spearman")$p.value
+P<-cor(x,y, method="pearson")
+Pp<-cor.test(x,y,method="pearson")$p.value
+
+#data$CaseID <- as.factor(data$CaseID)
+#data$X10 <- as.factor(data$X10)
+plot_result <- png(paste("/Volumes/Research_Data/Microbiome_analysis/CRC_combine_with_cufOff/Candidate_microbe/CORR_plot/Close_",category,"_",candidate,".png",sep = ""),width=3000,height=2400,res=300)
+plot_figure <- ggplot(data, aes(x=X01, y=X10)) + 
+  geom_point(size=2)+
+  xlab("Tumor samples") +
+  ylab("Blood_Samples") +
+  xlim(x_range)+
+  ylim(y_range)+
+  ggtitle(paste("Correlation_of",category,candidate,sep="_"))+
+  annotate(geom="text", x=0.20*max(x_range), y=max(y_range), label=paste("Spearman",S,sep = " = "), color="blue")+
+  annotate(geom="text", x=0.20*max(x_range), y=max(y_range)-(0.3*max(y_range)*0.3), label=paste("S-pvalue",Sp,sep = " = "), color="red")+
+  annotate(geom="text", x=0.20*max(x_range), y=max(y_range)-(0.3*max(y_range)*0.6), label=paste("Pearson",P,sep = " = "), color="blue")+
+  annotate(geom="text", x=0.20*max(x_range), y=max(y_range)-(0.3*max(y_range)*0.9), label=paste("P-pvalue",Pp,sep = " = "), color="red")+
+  theme(axis.text=element_text(size=14),
+        axis.title=element_text(size=14,face="bold"),
+        plot.title = element_text(color = "black", size = 20, face = "bold"))
+
+plot(plot_figure)
+dev.off()
+return(plot_result)
+}
+
+draw_correlation_plot("Phylum","Firmicutes",c(0,50000),c(0,75000))
+#-(0.3*max(y)0.6)
+
